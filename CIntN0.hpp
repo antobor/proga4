4d318b4d9f4cce2d30db12f24c9d092d59b35f77
antobor <63341967+antobor@users.noreply.github.com>
2020-05-16 21:23:17 +0300

    Update CIntN0.hpp

--- CIntN0.hpp
+++ CIntN0.hpp
@@ -17,5 +17,5 @@ class CIntN0: public CIntN{
         int output ();
         CIntN0 (int l,string number,string f="") : CIntN(l,number,f){};
         CIntN0 (int l) : CIntN(l){};
-        CIntN0 (const CIntN0& other) : CIntN(other){};
+        CIntN0 (const CIntN& other) : CIntN(other){};
 };
